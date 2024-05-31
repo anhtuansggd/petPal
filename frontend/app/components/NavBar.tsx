c6df9ab58b3b9f9c062169dade5ec90b1ed8a456
Tho_NguyenDuc <ductho892000@gmail.com>
2024-05-31 15:14:10 +0700

    Frontend (#9)
    * Initialize frontend
    
    * Navbar clean up
    
    * <fix>[frontend]: fix NavBar Component
    
    * <fix>[frontend]: Fix NavBar component
    
    ---------
    
    Co-authored-by: Rumis <20.21.9.12.1bibi@gmail.com>

---
+++ frontend/app/components/NavBar.tsx
@@ -0,0 +1,70 @@
+// "use client";
+// import { useState } from "react";
+import React from "react";
+import Link from "next/link";
+
+const NavBar = () => {
+  // const [isOpen, setIsOpen] = useState(false);
+  // const toggle = () => {
+  //   setIsOpen(!isOpen);
+  // };
+
+  //#28221a
+  return (
+    <>
+      <div className="w-full h-20 bg-white sticky top-0 border-y">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            <Link className="pl-10 pr-16 text-[#48c5bc]" href="/">
+              PETPAL LOGO
+            </Link>
+
+            <ul className="hidden md:flex gap-x-10 text-[#134848]">
+              <li>
+                <Link className="font-bold" href="/">
+                  <p>Home</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <p>Updates</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <p>Services</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <p>Features</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <p>About Us</p>
+                </Link>
+              </li>
+            </ul>
+
+            <ul className="hidden md:flex gap-x-8 items-center">
+              <li>
+                <Link href="/">EN</Link>
+              </li>
+              <li>
+                <button className="h-9 w-24 rounded-lg bg-[#5a35d6] text-white px-5">
+                  LogIn
+                </button>
+              </li>
+              <li>
+                <Link href="/">AVATAR</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
