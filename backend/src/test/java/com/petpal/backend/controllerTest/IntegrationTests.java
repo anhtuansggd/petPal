2ab4ea463856ad141d2c44804fde50824eabacae
LL Ayaya <107166414+LLAyaya@users.noreply.github.com>
2024-06-19 01:41:50 +0200

    fix (#17)
    * feat: account delete test &  add spring rest doc
    
    * feat(domain): refactor classes in domain
    
    * feat: jwt integration
    
    - implement login and validate jwt endpoints
    - integrate jwt with spring security
    
    * feat(controller): add endpoints for UserController and Auth Controller
    
    - change FetchType to avoid heavy query
    
    * feat: add PetController
    
    - more endpoints for UserController and PetsController
    
    * chore: dockerize backend
    
    * chore:  create .ignore and and fix docker volume
    
    * chore: fix docker compose for WSL subsystem
    
    * chore: fix docker file
    
    * feat(caregiver): add methods to find and save caregiver availability
    
    * feat: add methods to update and search for caregivers
    
    * test: write tests for AuthController
    
    * test: full flow test from register to search
    
    - need to refactor
    
    * test: parameterized intergration tests
    
    * feat: add services types to search endpoint
    
    * test: add tests for pet
    
    * feat: add Contract endpoints with date
    
    - Add service for contract endpoints
    - Add DateRange entity for Caregiver Availability
    - Add related tests
    
    * feat: add reject contract endpoint
    
    - with related tests
    
    * feat: add Chat endpoints
    
    - add related tests
    
    * feat: add upload image endpoints
    
    * feat: add get avatars for pet and user
    
    * feat: add get addtional images for pets
    
    * fix: delete code error
    
    * chore: make dockerfile
    
    ---------
    
    Co-authored-by: anhtuansggd <anhtuan.sggd@gmail.com>
    Co-authored-by: anhtuansggd <122171727+anhtuansggd@users.noreply.github.com>

--- backend/src/test/java/com/petpal/backend/controllerTest/IntegrationTests.java
+++ backend/src/test/java/com/petpal/backend/controllerTest/IntegrationTests.java
@@ -387,7 +387,6 @@ public class IntegrationTests {
                 "    \"message\": \"Hello everyone\"\n" +
                 "}";
 
-
         mockMvc.perform(post("/api/chat/send")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(jsonRequest))
@@ -395,28 +394,11 @@ public class IntegrationTests {
                 .andExpect(jsonPath("$").value("Message sent successfully"));
     }
 
-
-        mockMvc.perform(post("/api/chat/send")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonRequest))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").value("Message sent successfully"));
-    }
-
-
     @Order(22)
     @Test
     public void testGetMessages() throws Exception {
         Long senderId = 1L; // Assuming this is a valid sender ID
         Long receiverId = 2L; // Assuming this is a valid receiver ID
-
-
-    @Order(22)
-    @Test
-    public void testGetMessages() throws Exception {
-        Long senderId = 1L; // Assuming this is a valid sender ID
-        Long receiverId = 2L; // Assuming this is a valid receiver ID
-
 
         mockMvc.perform(get("/api/chat/messages")
                         .param("senderId", senderId.toString())
