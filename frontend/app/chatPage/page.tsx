4408e3f6536734e993f6f7adbe4845ffb2942fae
Rumis <56908557+MynameisBI@users.noreply.github.com>
2024-06-20 23:49:09 +0700

    feat: Adding Chat page (#21)
    * Initialize frontend
    
    * Navbar clean up
    
    * <fix>[frontend]: fix NavBar Component
    
    * <fix>[frontend]: Fix NavBar component
    
    * <feat>[frontend]: Add Chat page
    
    ---------
    
    Co-authored-by: Tho_NguyenDuc <ductho892000@gmail.com>

---
+++ frontend/app/chatPage/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { ACTION } from "next/dist/client/components/app-router-headers";
+import Chat from "./chat";
+import ChatSideBar from "./chatSideBar";
+import { useState, useEffect } from 'react'
+
+interface Account {
+  name: string
+}
+
+export default function ChatPage() {
+  const [accounts, setAccounts] = useState<Account[]>([{name: 'bibi'}, {name: 'bonbon'}, {name: 'bebe'}])
+  const [selectedAccount, setSelectedAccount] = useState(null)
+
+  const onAccountSelect = (account) => {
+    setSelectedAccount(account)
+  }
+
+  return (
+    <div className='flex grow w-full h-full'>
+      <ChatSideBar
+        accounts={accounts}
+        onAccountSelect={onAccountSelect}
+      />
+      <Chat 
+        selectedAccount={selectedAccount}
+      />
+    </div>
+  );
+}
