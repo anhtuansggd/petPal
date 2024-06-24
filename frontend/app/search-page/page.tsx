85f8ac4d5c4e73d5982acce481bf129dea91737d
Tho_NguyenDuc <ductho892000@gmail.com>
2024-06-25 00:27:55 +0700

    Frontend (#26)
    * Initialize frontend
    
    * Navbar clean up
    
    * <fix>[frontend]: fix NavBar Component
    
    * <fix>[frontend]: Fix NavBar component
    
    * <feat>[frontend]: Add Chat page
    
    * new components
    
    home-page
    search-page
    contect-page
    
    * Update package-lock.json
    
    * components rename
    
    * Fix: add default type
    
    * lastest-working frontend
    
    * Update page.tsx
    
    * Minor syntax fix
    
    ---------
    
    Co-authored-by: Rumis <20.21.9.12.1bibi@gmail.com>
    Co-authored-by: MynameisBI <56908557+MynameisBI@users.noreply.github.com>

--- frontend/app/search-page/page.tsx
+++ frontend/app/search-page/page.tsx
@@ -1,19 +1,13 @@
-import NavBar from "../components/nav-bar";
 import FAQ from "./faq";
 import SearchBar from "./search-bar";
 import Test from "./test";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col">
       <SearchBar />
-<<<<<<< HEAD
       <FAQ />
       {/* <Test /> */}
-=======
-      {/* <FAQ /> */}
-      <Test />
->>>>>>> main
-    </main>
+    </div>
   );
 }
