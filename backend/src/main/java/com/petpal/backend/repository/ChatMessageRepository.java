13a3bf402adb8f13f9ee6fc142cf7efe6104097b
LL Ayaya <107166414+LLAyaya@users.noreply.github.com>
2024-06-25 15:35:44 +0200

    feat: getContact API and update IntegrationTest (#31)
    * feat: account delete test &  add spring rest doc
    
    * feat(domain): refactor classes in domain
    
    * feat: jwt integration
    
    - implement login and validate jwt endpoints
    - integrate jwt with spring security
    
    * feat(controller): add endpoints for UserController and Auth Controller
    
    - change FetchType to avoid heavy query
    
    * feat: add PetController
    
    - more endpoints for UserController and PetsController
    
    * chore: dockerize backend
    
    * chore:  create .ignore and and fix docker volume
    
    * chore: fix docker compose for WSL subsystem
    
    * chore: fix docker file
    
    * feat(caregiver): add methods to find and save caregiver availability
    
    * feat: add methods to update and search for caregivers
    
    * test: write tests for AuthController
    
    * test: full flow test from register to search
    
    - need to refactor
    
    * test: parameterized intergration tests
    
    * feat: add services types to search endpoint
    
    * test: add tests for pet
    
    * feat: add Contract endpoints with date
    
    - Add service for contract endpoints
    - Add DateRange entity for Caregiver Availability
    - Add related tests
    
    * feat: add reject contract endpoint
    
    - with related tests
    
    * feat: add Chat endpoints
    
    - add related tests
    
    * feat: add upload image endpoints
    
    * feat: add get avatars for pet and user
    
    * feat: add get addtional images for pets
    
    * fix: delete code error
    
    * chore: make dockerfile
    
    * chore: set docker compose to latest backend
    
    * fix: change typo in docker compose
    
    * doc: document for test case from 1 to 9
    
    * doc: document for test case and adjust integrationtest
    
    * chore: final IntergrationTest for API and adoc
    
    * doc: change documentation description
    
    * chore: update dependency to compatible with Macos
    
    * config: add nginx and replace nodejs version
    
    * feat: getContact API and test
    
    * minor update
    
    * chore: update test
    
    ---------
    
    Co-authored-by: anhtuansggd <anhtuan.sggd@gmail.com>
    Co-authored-by: anhtuansggd <122171727+anhtuansggd@users.noreply.github.com>

--- backend/src/main/java/com/petpal/backend/repository/ChatMessageRepository.java
+++ backend/src/main/java/com/petpal/backend/repository/ChatMessageRepository.java
@@ -4,9 +4,14 @@ package com.petpal.backend.repository;
 import com.petpal.backend.domain.Message;
 import com.petpal.backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<Message, Long> {
     List<Message> findBySenderAndReceiver(User sender, User receiver);
+
+    @Query("SELECT DISTINCT m.receiver FROM Message m WHERE m.sender = :sender")
+    List<User> findDistinctReceiverBySender(@Param("sender") User sender);
 }
