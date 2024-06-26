4a443cc88b3191ba2f60fc9795f307fea90d1a52
anhtuansggd <122171727+anhtuansggd@users.noreply.github.com>
2024-06-26 18:08:48 +0700

    feat: refactor Chat (#34)
    * feat: account delete test &  add spring rest doc
    
    * feat(domain): refactor classes in domain
    
    * feat: jwt integration
    
    - implement login and validate jwt endpoints
    - integrate jwt with spring security
    
    * feat(controller): add endpoints for UserController and Auth Controller
    
    - change FetchType to avoid heavy query
    
    * feat: add PetController
    
    - more endpoints for UserController and PetsController
    
    * chore: dockerize backend
    
    * chore:  create .ignore and and fix docker volume
    
    * chore: fix docker compose for WSL subsystem
    
    * chore: fix docker file
    
    * feat(caregiver): add methods to find and save caregiver availability
    
    * feat: add methods to update and search for caregivers
    
    * test: write tests for AuthController
    
    * test: full flow test from register to search
    
    - need to refactor
    
    * test: parameterized intergration tests
    
    * feat: add services types to search endpoint
    
    * test: add tests for pet
    
    * feat: add Contract endpoints with date
    
    - Add service for contract endpoints
    - Add DateRange entity for Caregiver Availability
    - Add related tests
    
    * feat: add reject contract endpoint
    
    - with related tests
    
    * feat: add Chat endpoints
    
    - add related tests
    
    * feat: add upload image endpoints
    
    * feat: add get avatars for pet and user
    
    * feat: add get addtional images for pets
    
    * fix: delete code error
    
    * chore: make dockerfile
    
    * chore: set docker compose to latest backend
    
    * fix: change typo in docker compose
    
    * doc: document for test case from 1 to 9
    
    * doc: document for test case and adjust integrationtest
    
    * chore: final IntergrationTest for API and adoc
    
    * doc: change documentation description
    
    * chore: update dependency to compatible with Macos
    
    * config: add nginx and replace nodejs version
    
    * feat: getContact API and test
    
    * minor update
    
    * chore: update test
    
    * feat: refactor Chat
    
    ---------
    
    Co-authored-by: LL Ayaya <doanphuocthong2002@gmail.com>
    Co-authored-by: LL Ayaya <107166414+LLAyaya@users.noreply.github.com>

---
+++ backend/src/main/java/com/petpal/backend/service/ChatMessageService.java
@@ -0,0 +1,46 @@
+package com.petpal.backend.service;
+
+import com.petpal.backend.domain.Message;
+import com.petpal.backend.domain.User;
+import com.petpal.backend.dto.MessageRequest;
+import com.petpal.backend.repository.ChatMessageRepository;
+import com.petpal.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ChatMessageService {
+    @Autowired
+    private ChatMessageRepository chatMessageRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Message saveMessage(MessageRequest messageRequest){
+        User sender = userRepository.findById(messageRequest.getSenderId()).orElseThrow(() -> new RuntimeException("Sender not found"));
+        sender.setPassword(null);
+        User receiver = userRepository.findById(messageRequest.getReceiverId()).orElseThrow(() -> new RuntimeException("Receiver not found"));
+        sender.setPassword(null);
+        Message message = new Message();
+        message.setSender(sender);
+        message.setReceiver(receiver);
+        message.setMessage(messageRequest.getMessage());
+        return chatMessageRepository.save(message);
+    }
+
+    public List<Message> getMessage(Long senderId, long receiverId){
+        User sender = userRepository.findById(senderId).orElseThrow(() -> new RuntimeException("Sender not found"));
+        User receiver = userRepository.findById(receiverId).orElseThrow(() -> new RuntimeException("Receiver not found"));
+        List<Message> messages = chatMessageRepository.findBySenderAndReceiver(sender, receiver);
+        return messages;
+    }
+
+    public List<User> getContacts(Long senderId){
+        User sender = userRepository.findById(senderId).orElseThrow(() -> new RuntimeException("User not found"));
+        List<User> contacts = chatMessageRepository.findDistinctReceiverBySender(sender);
+        return contacts;
+
+    }
+}
