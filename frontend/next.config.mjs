01c47f2e76d49b14b7003409728b8e762e199aa5
anhtuansggd <122171727+anhtuansggd@users.noreply.github.com>
2024-07-02 12:14:06 +0700

    fix: error  (#51)
    * Initialize frontend
    
    * Navbar clean up
    
    * <fix>[frontend]: fix NavBar Component
    
    * <fix>[frontend]: Fix NavBar component
    
    * <feat>[frontend]: Add Chat page
    
    * new components
    
    home-page
    search-page
    contect-page
    
    * Update package-lock.json
    
    * components rename
    
    * Fix: add default type
    
    * lastest-working frontend
    
    * Update page.tsx
    
    * Minor syntax fix
    
    * config: add user to frontend image
    
    * config: set nginx settings
    
    * fix: remove duplicate image
    
    * config: try to add ssl-service
    
    * config: nginx with ssl
    
    * Add fetchService.js
    
    * config: routing to backend
    
    * sitter-info fix
    
    * Register API Call Cors
    
    * Reroutes into (footer) and (no-footer)
    
    * Fix /chat-page layout
    
    Now /chat-page takes the whole screen
    
    * Add message style for chat component in /chat-page
    
    * Register Function done
    
    * update: log-in page
    
    * Fix navbar avatar
    
    * Implement backend
    
    * <feat> Chat is scrollable
    
    * feat: popup notification
    
    * Last working
    
    * Remove (footer) and (non-footer) route
    
    * Relayout /chat-page
    
    * Implement /chat-page user flow
    
    * Send message when Enter key down
    
    * feat: mapPopup
    
    * feat: updateSearchPage
    
    * Last working dt89
    
    * Locate Me Error
    
    * fix caregiver profile
    
    * feat: fix sign, log and update user-profile
    
    * last working: map popup + contract
    
    * feat: update Contract
    
    * Update nav-bar to current pages
    
    * complete component: contract
    
    * Search bar fix
    
    * fix: remove node_modules
    
    * chore: update git ignore
    
    * fix: Typescript error in map and refactor package
    
    * hotfix
    
    ---------
    
    Co-authored-by: Rumis <20.21.9.12.1bibi@gmail.com>
    Co-authored-by: Tho_NguyenDuc <ductho892000@gmail.com>
    Co-authored-by: MynameisBI <56908557+MynameisBI@users.noreply.github.com>
    Co-authored-by: LL Ayaya <doanphuocthong2002@gmail.com>

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
       return [
         {
           source: '/api/:path*',
-          destination: 'http://localhost:8081/api/:path*' 
+          destination: 'http://backend:8081/api/:path*' 
         }
       ]
     }
