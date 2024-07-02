3770570aca95fc07baef3ce776ea9d396fb1a4f3
anhtuansggd <122171727+anhtuansggd@users.noreply.github.com>
2024-07-02 13:03:17 +0700

    fix: turn of cors and fix api routing

--- frontend/app/log-in/log-in.tsx
+++ frontend/app/log-in/log-in.tsx
@@ -24,7 +24,7 @@ export default function Login() {
     };
 
     try {
-      const res = await fetch("http://backend:8081/api/auth/login", {
+      const res = await fetch("/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(User),
